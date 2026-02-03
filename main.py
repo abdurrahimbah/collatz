41401983573705858a32f9226927b491249925c9
Abdur Rahim <abdurrahim@universe>
2026-02-03 17:18:55 +0600

    replace collatz() with collatz_simple()

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2026 Abdur Rahim
 # Licensed under GPL-3.0-or-later
 
-from collatz import collatz
+from collatz import collatz_simple
 from nth_prime import nth_prime
 
 n = int(input("Hello. Please enter a number: "))
@@ -10,8 +10,6 @@ prime_number = nth_prime(n)
 print(str(n) + "th prime number is : " + str(prime_number))
 
 print("The Collatz Sequence starting at " + str(n) + " is: ")
-
-points = collatz(prime_number)
-print(points)
+collatz_simple(n)
 
 
