55f8af44b3b8a8177713e56e42e69140c41f844a
Abdur Rahim <abdurrahim@universe>
2026-02-03 17:03:17 +0600

    rename the function for the siple Collatz sequence in collatz_simple.py to collatz_simple(), move the function from collatz_simple.py to collatz.py to keep things clean and minimal

--- collatz.py
+++ collatz.py
@@ -17,3 +17,19 @@ def collatz(n, collatz_points = None):
 	elif n % 2 == 0:
 		collatz_points.append(n // 2)
 		return collatz(n // 2, collatz_points)
+		
+
+def collatz_simple(n): 
+	points = []
+	print(n)
+	points.append(n)
+	while n > 1:
+		if n % 2 == 0: 
+			n = int(n / 2)
+			print(n)
+			points.append(n)
+		else: 
+			n = 3 * n + 1
+			print(n)
+			points.append(n)
+	return points
