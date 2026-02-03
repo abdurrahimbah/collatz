36c4729e4e08943585ef9c1e1ca285da22815221
Abdur Rahim <abdurrahim@universe>
2026-02-03 16:57:04 +0600

    functionize the simple Collatz sequence

--- collatz_simple.py
+++ collatz_simple.py
@@ -1,13 +1,15 @@
-n = 10000
-points = []
-
-while n > 1:
-	if n % 2 == 0: 
-		n = int(n / 2)
-		print(n)
-		points.append(n)
-	else: 
-		n = 3 * n + 1
-		print(n)
-		points.append(n)
-print(points)
+def collatz(n): 
+	points = []
+	while n > 1:
+		if n % 2 == 0: 
+			n = int(n / 2)
+			print(n)
+			points.append(n)
+		else: 
+			n = 3 * n + 1
+			print(n)
+			points.append(n)
+	print(points)
+	return points
+	
+collatz(100)
