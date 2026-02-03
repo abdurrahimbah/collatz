417e00c7d4a5a67723c6f936158c27a0fbe83119
Abdur Rahim <abdurrahim@universe>
2026-02-03 14:37:57 +0600

    copy main.py to main.testing.py

---
+++ main_testing.py
@@ -0,0 +1,15 @@
+# Collatz Conjecture
+# Copyright (C) 2026 Abdur Rahim
+# Licensed under GPL-3.0-or-later
+
+from collatz import collatz
+from nth_prime import nth_prime
+
+n = int(input("Hello. Please enter a number: "))
+prime_number = nth_prime(n)
+print(str(n) + "th prime number is : " + str(prime_number))
+
+points = collatz(prime_number)
+print(points)
+
+
