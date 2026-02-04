ce4fa553f61435957ff16e927b680f0e8a8134f6
Abdur Rahim <abdurrahim@universe>
2026-02-04 18:43:06 +0600

    use collatz_simple_print() from collatz.py instead of collatz_simple() [we do not need the list output from collatz_simple() for main.py to function]

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2026 Abdur Rahim
 # Licensed under GPL-3.0-or-later
 
-from collatz import collatz_simple
+from collatz import collatz_simple_print
 from nth_prime import nth_prime
 
 n = int(input("Hello. Please enter a number: "))
@@ -10,6 +10,6 @@ prime_number = nth_prime(n)
 print(str(n) + "th prime number is : " + str(prime_number))
 
 print("The Collatz Sequence starting at " + str(prime_number) + " is: ")
-collatz_simple(prime_number)
+collatz_simple_print(prime_number)
 
 
